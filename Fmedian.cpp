0e5c725f3ff355470e9f754fffcbacfe4e23cd14
StarxSky (Mochi) <62407841+StarxSky@users.noreply.github.com>
2026-09-22 15:24:56 +0800

    fix the clang-format probelm

--- Fmedian.cpp
+++ Fmedian.cpp
@@ -1,7 +1,6 @@
 #include <cmath>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
--- zigzag_conversion.cpp
+++ zigzag_conversion.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <string>
-#include <vector> 
-
 using namespace std;
 
 string convert(string &s, int &numRows) {
